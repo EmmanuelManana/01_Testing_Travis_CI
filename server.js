876f857d23b29df21d7181e9c15a7c5ca43c7f17
EmmanuelManana <emanana@student.wethinkcode.co.za>
2020-12-20 16:56:12 +0200

    env

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const USERNAME = process.env.POSTGRESS_USERNAME;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
+    console.log(`${USERNAME}`)
     console.log('Containers rule!');
     await sleep(5000);
   }
